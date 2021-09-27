f98be3562266a5eec29a15394d8859bf3d395371
Manuel Esparza <manoloesparta@gmail.com>
2021-09-27 15:34:21 -0700

    little bug repaired

--- pages/src/app.js
+++ pages/src/app.js
@@ -1,4 +1,4 @@
-import './styles.css'
+import "./styles.css"
 
 const API = "https://shrt.manoloesparta.workers.dev"
 
@@ -6,7 +6,7 @@ const urlElement = document.getElementById("url")
 const shortenButton = document.getElementById("shorten")
 const luckyButton = document.getElementById("lucky")
 
-shortenButton.addEventListener('click', () => {
+shortenButton.addEventListener("click", async () => {
   const value = urlElement.value;
 
   const options = {
@@ -15,19 +15,42 @@ shortenButton.addEventListener('click', () => {
     headers: {"Content-Type": "application/json"},
   }
 
-  fetch(`${API}/short`, options)
-    .then(res => res.json())
-    .catch(() => Swal.fire({ icon: 'error', title: 'Oops...', text: 'Did you check that site is up?'}))
-    .then(data => Swal.fire({ icon: 'success', title: 'Finally!', html: `Here is your shortened link (probably not): <br><strong>${API}/${data.hash}</strong>` }))
+  try {
+    const res = await fetch(`${API}/short`, options)
+    const data = await res.json()
+
+    const htmlContent = `Here is your shortened link (probably not): <br><strong id="link">${API}/${data.hash}</strong>` 
+
+    Swal.fire({ 
+      icon: "success", 
+      title: "Finally!", 
+      html: htmlContent
+    })
+  } catch {
+    const htmlContent = "Did you check that site is up? <br>Don\'t forget to add the <strong>\"http(s):\"</strong> part."
+
+    Swal.fire({ 
+      icon: "error", 
+      title: "Oops...", 
+      html: htmlContent
+    })
+  }
 })
 
-luckyButton.addEventListener('click', () => {
+luckyButton.addEventListener("click", async () => {
   const options = {
     method: "GET"
   }
-  
-  fetch(`${API}/lucky`, options)
-    .then(res => res.json())
-    .catch(() => Swal.fire({ icon: 'error', title: 'Oops...', text: 'Something went wrong, please try again'}))
-    .then(({ url }) => window.location.replace(url))
+
+  try {
+    const res = await fetch(`${API}/lucky`, options)
+    const { url } = await res.json()
+    window.location.replace(url)
+  } catch {
+    Swal.fire({ 
+      icon: "error", 
+      title: "Oops...", 
+      text: "Something went wrong, please try again"
+    })
+  }
 })
